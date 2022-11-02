43b65f310c6c97f6b4d70f025e985a8837bc6e7a
Brainybrian316 <brainybrian316@gmail.com>
2022-11-02 16:28:24 -0400

    home page for notes

---
+++ app/notes/[id]/page.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+import styles from './Notes.module.css';
+// import CreateNote from './Create'
+export default async function NotesPage() {
+	// const notes = await getNotes();
+	return (
+		<div>
+			<h1>Notes</h1>
+			<div className={styles.grid}>
+				{notes?.map((note) => {
+					return <Note key={note.id} note={note} />;
+				})}
+			</div>
+
+			<CreateNote />
+		</div>
+	);
+}
+
+function Note({ note }: any) {
+	const { id, title, content, created } = note || {};
+
+	return (
+		<Link href={`/notes/${id}`}>
+			<div className={styles.note}>
+				<h2>{title}</h2>
+				<h5>{content}</h5>
+				<p>{created}</p>
+			</div>
+		</Link>
+	);
+}
---
+++ app/notes/page.tsx
@@ -0,0 +1,3 @@
+export default async function NotePage({ params }: any) {
+	return <></>;
+}
