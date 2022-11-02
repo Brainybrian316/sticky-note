850f0821defc5ff9816f97a0a87e90ac654fa4f6
Brainybrian316 <brainybrian316@gmail.com>
2022-11-02 16:29:55 -0400

    set up notes database

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,14 @@
 import Link from 'next/link';
 import styles from './Notes.module.css';
 // import CreateNote from './Create'
+
+async function getNotes() {
+	const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', {
+		cache: 'no-store',
+	});
+	const data = await res.json();
+	return data?.items as any[];
+}
 export default async function NotesPage() {
 	// const notes = await getNotes();
 	return (
